c1ade005865bb3e8ae79d7c7d4868313d8caadf8
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-18 03:51:47 -0300

    test: add a basic test to the DbNewMarket

---
+++ tests/application/usecases/db-new-market.spec.ts
@@ -0,0 +1,26 @@
+import { DbNewMarket, NewMarketRepository } from '@/application';
+import { Market } from '@/domain';
+
+describe('DbNewMarket', () => {
+  it('shoud create a new Market and return success', async () => {
+    // Arrange
+    class MockedNewMarketRepository implements NewMarketRepository {
+      create = (market: Market): Promise<void> => {
+        return Promise.resolve();
+      };
+    }
+
+    const repository = new MockedNewMarketRepository();
+    const sut = new DbNewMarket(repository);
+
+    // Act
+    const response = await sut.execute({
+      name: 'Assai Carapicuiba',
+      user: 'tiagoluizpoli@gmail.com',
+    });
+
+    // Assert
+
+    expect(response.isRight()).toBe(true);
+  });
+});
