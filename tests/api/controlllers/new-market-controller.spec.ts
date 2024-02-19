47c18f726967a897e1ae884293a5fcb6863b1c98
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-19 03:50:05 -0300

    test: ensure NewMarketController returns 400 if Market already exists

--- tests/api/controlllers/new-market-controller.spec.ts
+++ tests/api/controlllers/new-market-controller.spec.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata';
 import { makeSut } from './mocks';
 
+import { Market, MarketAlreadyExistsError } from '@/domain';
+import { badRequest } from '@/api';
+
 describe('NewMarketController', () => {
   it('shoud call NewMarket usecase with correct values', async () => {
     // Arrange
@@ -21,10 +24,26 @@ describe('NewMarketController', () => {
     });
   });
 
-  it('shoud return 422 - UnprocessableEntity if Market already exists', () => {
+  it('shoud return 400 - BadRequest if Market already exists', async () => {
     // Arrange
+    const { sut, mockedMarketRepository } = makeSut();
+
+    vi.spyOn(mockedMarketRepository, 'getByCode').mockResolvedValueOnce(
+      Market.create({
+        name: 'Assai Carapicuiba',
+        createdAt: new Date(),
+        createdBy: 'tiagoluizpoli@gmail.com',
+      }),
+    );
+
     // Act
+    const response = await sut.handle({
+      user: 'tiagoluizpoli@gmail.com',
+      name: 'Assai Carapicuiba',
+    });
+
     // Assert
+    expect(response).toEqual(badRequest(new MarketAlreadyExistsError()));
   });
 
   it('shoud return 200 - Ok on success', () => {
