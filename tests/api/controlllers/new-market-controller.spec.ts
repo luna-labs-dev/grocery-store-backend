1874daa338429d1286bf9e02eaa493716741f46c
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-19 03:59:28 -0300

    test: ensure NewMarketController returns 200 on success

--- tests/api/controlllers/new-market-controller.spec.ts
+++ tests/api/controlllers/new-market-controller.spec.ts
@@ -3,6 +3,7 @@ import { makeSut } from './mocks';
 
 import { Market, MarketAlreadyExistsError } from '@/domain';
 import { badRequest } from '@/api';
+import { nameToCode } from '@/domain/helper';
 
 describe('NewMarketController', () => {
   it('shoud call NewMarket usecase with correct values', async () => {
@@ -46,9 +47,19 @@ describe('NewMarketController', () => {
     expect(response).toEqual(badRequest(new MarketAlreadyExistsError()));
   });
 
-  it('shoud return 200 - Ok on success', () => {
+  it('shoud return 200 - Ok on success', async () => {
     // Arrange
+    const { sut } = makeSut();
+
     // Act
+    const response = await sut.handle({
+      user: 'tiagoluizpoli@gmail.com',
+      name: 'Assai Carapicuiba',
+    });
+
     // Assert
+    expect(response.statusCode).toBe(200);
+    expect(response.body.name).toBe('Assai Carapicuiba');
+    expect(response.body.code).toBe(nameToCode('Assai Carapicuiba'));
   });
 });
