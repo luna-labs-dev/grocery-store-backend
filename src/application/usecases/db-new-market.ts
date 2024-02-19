9cb94b7acd5bd68e67e91a137adaddc5097d3466
Tiago Poli <tiago.poli@stoneage.com.br>
2024-02-19 00:00:14 -0300

    feat: ensure DbNewMarket returns MarketAlreadyExistsError if Market exists

--- src/application/usecases/db-new-market.ts
+++ src/application/usecases/db-new-market.ts
@@ -1,28 +1,39 @@
-import { NewMarketRepository } from '../contracts';
-
 import {
   Either,
   left,
   Market,
+  MarketAlreadyExistsError,
   NewMarket,
+  NewMarketErrors,
   NewMarketParams,
   NewMarketResult,
   right,
   UnexpectedError,
 } from '@/domain';
+import { GetMarketByCodeRepository, NewMarketRepository } from '@/application';
+
+export type NewMarketRepositories = NewMarketRepository & GetMarketByCodeRepository;
 
 export class DbNewMarket implements NewMarket {
-  constructor(private readonly repository: NewMarketRepository) {}
+  constructor(private readonly repository: NewMarketRepositories) {}
   execute = async ({
     name,
     user,
-  }: NewMarketParams): Promise<Either<UnexpectedError, NewMarketResult>> => {
+  }: NewMarketParams): Promise<Either<NewMarketErrors, NewMarketResult>> => {
     try {
       const market = Market.create({
         name,
         createdAt: new Date(),
         createdBy: user,
       });
+
+      const marketExists = await this.repository.getByCode({
+        code: market.code as string,
+      });
+
+      if (marketExists) {
+        return left(new MarketAlreadyExistsError());
+      }
 
       await this.repository.create(market);
 
