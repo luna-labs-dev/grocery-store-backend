fab33180683083c2179177061b569c15e20ae7f4
Tiago Poli <tiago.poli@stoneage.com.br>
2024-02-19 00:00:27 -0300

    test: ensure DbNewMarket returns MarketAlreadyExistsError if Market exists

--- tests/application/usecases/db-new-market.spec.ts
+++ tests/application/usecases/db-new-market.spec.ts
@@ -1,20 +1,60 @@
-import { DbNewMarket, NewMarketRepository } from '@/application';
-import { Market } from '@/domain';
+import { DbNewMarket, GetMarketByCodeRepositoryParams, NewMarketRepositories } from '@/application';
+import { Market, MarketAlreadyExistsError, NewMarket, NewMarketErrors } from '@/domain';
+
+const mockRepository = (): NewMarketRepositories => {
+  class MockedNewMarketRepository implements NewMarketRepositories {
+    getByCode = (params: GetMarketByCodeRepositoryParams): Promise<Market | undefined> => {
+      return Promise.resolve(undefined);
+    };
+
+    create = (market: Market): Promise<void> => {
+      return Promise.resolve();
+    };
+  }
+
+  return new MockedNewMarketRepository();
+};
+
+interface SutResult {
+  sut: NewMarket;
+  repository: NewMarketRepositories;
+}
+
+const makeSut = (): SutResult => {
+  const repository = mockRepository();
+  const sut = new DbNewMarket(repository);
+
+  return { sut, repository };
+};
 
 describe('DbNewMarket', () => {
-  it.todo('should return MarketAlreadyExists when code is found on database');
+  it('should return MarketAlreadyExistsError when code is found on database', async () => {
+    // Arrange
+    const { sut, repository } = makeSut();
+    vi.spyOn(repository, 'getByCode').mockResolvedValueOnce(
+      Market.create({
+        name: 'Assai Carapicuiba',
+        createdAt: new Date(),
+        createdBy: 'tiagoluizpoli@gmail.com',
+      }),
+    );
+    // Act
+    const response = await sut.execute({
+      name: 'Assai Carapicuiba',
+      user: 'tiagoluizpoli@gmail.com',
+    });
+
+    // Assert
+
+    expect(response.isLeft()).toBe(true);
+    expect(response.value as NewMarketErrors).toEqual(new MarketAlreadyExistsError());
+  });
+
+  it.todo('should return UnexpectedError if repository throws');
 
   it('shoud create a new Market and return success', async () => {
     // Arrange
-    class MockedNewMarketRepository implements NewMarketRepository {
-      create = (market: Market): Promise<void> => {
-        return Promise.resolve();
-      };
-    }
-
-    const repository = new MockedNewMarketRepository();
-    const sut = new DbNewMarket(repository);
-
+    const { sut } = makeSut();
     // Act
     const response = await sut.execute({
       name: 'Assai Carapicuiba',
