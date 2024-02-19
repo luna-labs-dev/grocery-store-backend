e62fd6957cfdeb94937e727a8a4d0ba0358ecefd
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-19 03:49:07 -0300

    refactor: change marketAlreadyExists error map to badRequest

--- src/api/helpers/error-mapper.ts
+++ src/api/helpers/error-mapper.ts
@@ -1,9 +1,9 @@
-import { notFound, serverError, unprocessableEntity } from './http-helper';
+import { badRequest, notFound, serverError } from './http-helper';
 
 import { UseCaseError } from '@/domain';
 
 export const errorMapper: Record<string, any> = {
   UNEXPECTED_ERROR: (error: UseCaseError) => serverError(error),
-  MARKET_ALREADY_EXISTS_ERROR: (error: UseCaseError) => unprocessableEntity(error),
+  MARKET_ALREADY_EXISTS_ERROR: (error: UseCaseError) => badRequest(error),
   MARKET_NOT_FOUND_ERROR: (error: UseCaseError) => notFound(error),
 };
