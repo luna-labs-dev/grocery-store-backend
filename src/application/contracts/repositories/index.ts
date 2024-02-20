a57818c918a08867f2b7d1549474e601b42264bc
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-20 00:06:10 -0300

    feat: ensure GetMarketByIdRepository is called with correct values

--- src/application/contracts/repositories/index.ts
+++ src/application/contracts/repositories/index.ts
@@ -1,7 +1,11 @@
 import { GetMarketByCodeRepository } from './get-market-by-code-repository';
+import { GetMarketByIdRepository } from './get-market-by-id-repository';
 import { NewMarketRepository } from './new-market-repository';
+
+export type MarketRepositories = NewMarketRepository &
+  GetMarketByCodeRepository &
+  GetMarketByIdRepository;
 
 export * from './new-market-repository';
 export * from './get-market-by-code-repository';
-
-export type MarketRepositories = NewMarketRepository & GetMarketByCodeRepository;
+export * from './get-market-by-id-repository';
--- src/application/usecases/db-update-market.ts
+++ src/application/usecases/db-update-market.ts
@@ -1,3 +1,7 @@
+import { inject, injectable } from 'tsyringe';
+
+import { GetMarketByIdRepository } from '../contracts';
+
 import {
   Either,
   left,
@@ -7,13 +11,31 @@ import {
   UpdateMarketErrors,
   UpdateMarketParams,
 } from '@/domain';
+import { injection } from '@/main/di/injection-codes';
 
+export type UpdateMarketRepositories = GetMarketByIdRepository;
+
+const { infra } = injection;
+@injectable()
 export class DbUpdateMarket implements UpdateMarket {
+  constructor(
+    @inject(infra.marketRepositories) private readonly repositories: UpdateMarketRepositories,
+  ) {}
+
   execute = async ({
     name,
     marketId,
   }: UpdateMarketParams): Promise<Either<UpdateMarketErrors, Market>> => {
     // GetMarketById
+    await this.repositories.getById({ id: marketId });
+
+    // Return Market not found if No market is returned
+
+    // Update Market Entity with new values
+
+    // Update Market in the Database
+
+    // Return Updated Market
     return left(new UnexpectedError());
   };
 }
