856f9c6292d7344e7cb49b183e42d2bb9dc2683d
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-19 23:11:57 -0300

    test: add UpdateMarket todo tests

---
+++ tests/application/usecases/db-update-market.spec.ts
@@ -0,0 +1,37 @@
+describe('DbUpdateMarket', () => {
+  it.todo('shoud call GetMarketByIdRepository with correct id', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it.todo('shoud return UnexpectedError if GetMarketByIdRepository throws', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it.todo('shoud return MarketNotFoundError if GetMarketByIdRepository returns undefined', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it.todo('shoud call UpdateMarketRepository with correct values', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it.todo('shoud return UnexpectedError if UpdateMarketRepository throws', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it.todo('shoud return an updated Market on success', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+});
