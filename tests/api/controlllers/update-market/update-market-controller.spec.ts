a0a443252236dbfba7d9394465f9e46652827120
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-20 01:05:35 -0300

    test: add UpdateMarketController todo tests

---
+++ tests/api/controlllers/update-market/update-market-controller.spec.ts
@@ -0,0 +1,25 @@
+describe('UpdateMarketController', () => {
+  it.todo('shoud call UpdateMarket usecase with correct values', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it.todo('shoud return 404 - NotFound if UpdateMarket usecase return MarketNotFoundError', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it.todo('shoud return 500 - ServerError if UpdateMarket usecase return UnexpectedError ', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+
+  it.todo('shoud return 200 - Ok on success', () => {
+    // Arrange
+    // Act
+    // Assert
+  });
+});
