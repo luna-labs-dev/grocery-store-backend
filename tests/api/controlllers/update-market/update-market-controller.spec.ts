517123d94131a0ad589017370c7e690aea873eab
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-20 02:06:20 -0300

    test: ensure UpdateMarketController returns 500 if UpdateMarket usecase returns UnexpectedError

--- tests/api/controlllers/update-market/update-market-controller.spec.ts
+++ tests/api/controlllers/update-market/update-market-controller.spec.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata';
 import { makeSut, mockedUpdateMarketControllerParams } from './mocks';
 
-import { left, MarketNotFoundError } from '@/domain';
-import { notFound } from '@/api';
+import { left, MarketNotFoundError, UnexpectedError } from '@/domain';
+import { notFound, serverError } from '@/api';
 
 describe('UpdateMarketController', () => {
   it('shoud call UpdateMarket usecase with correct values', async () => {
@@ -31,10 +31,17 @@ describe('UpdateMarketController', () => {
     expect(response).toEqual(notFound(new MarketNotFoundError()));
   });
 
-  it.todo('shoud return 500 - ServerError if UpdateMarket usecase return UnexpectedError ', () => {
+  it('shoud return 500 - ServerError if UpdateMarket usecase return UnexpectedError ', async () => {
     // Arrange
+    const { sut, mockedUpdateMarket } = makeSut();
+    vi.spyOn(mockedUpdateMarket, 'execute').mockResolvedValueOnce(left(new UnexpectedError()));
+    const { params } = mockedUpdateMarketControllerParams();
+
     // Act
+    const response = await sut.handle(params);
+
     // Assert
+    expect(response).toEqual(serverError(new UnexpectedError()));
   });
 
   it.todo('shoud return 200 - Ok on success', () => {
