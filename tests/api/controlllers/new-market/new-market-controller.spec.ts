4d4422be309385ec7cd3bd886ce7d49b4440660e
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-20 01:04:28 -0300

    test: add missing test to NewMarketController

--- tests/api/controlllers/new-market/new-market-controller.spec.ts
+++ tests/api/controlllers/new-market/new-market-controller.spec.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata';
 import { makeSut } from './mocks';
 
-import { Market, MarketAlreadyExistsError } from '@/domain';
-import { badRequest } from '@/api';
+import { left, Market, MarketAlreadyExistsError, UnexpectedError } from '@/domain';
+import { badRequest, serverError } from '@/api';
 import { nameToCode } from '@/domain/helper';
 
 describe('NewMarketController', () => {
@@ -45,6 +45,22 @@ describe('NewMarketController', () => {
 
     // Assert
     expect(response).toEqual(badRequest(new MarketAlreadyExistsError()));
+  });
+
+  it('shoud return 500 - ServerError NewMarket usecase return UnexpectedError', async () => {
+    // Arrange
+    const { sut, mockedNewMarket } = makeSut();
+
+    vi.spyOn(mockedNewMarket, 'execute').mockResolvedValueOnce(left(new UnexpectedError()));
+
+    // Act
+    const response = await sut.handle({
+      user: 'tiagoluizpoli@gmail.com',
+      name: 'Assai Carapicuiba',
+    });
+
+    // Assert
+    expect(response).toEqual(serverError(new UnexpectedError()));
   });
 
   it('shoud return 200 - Ok on success', async () => {
