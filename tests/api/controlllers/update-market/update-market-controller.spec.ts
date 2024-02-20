405737e64a18a7ad999c11ca2afa87fd101fa0cc
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-20 01:55:01 -0300

    test: ensure UpdateMarketController calls UpdateMarket uscase with correct values

--- tests/api/controlllers/update-market/update-market-controller.spec.ts
+++ tests/api/controlllers/update-market/update-market-controller.spec.ts
@@ -1,8 +1,18 @@
+import 'reflect-metadata';
+import { makeSut, mockedUpdateMarketControllerParams } from './mocks';
+
 describe('UpdateMarketController', () => {
-  it.todo('shoud call UpdateMarket usecase with correct values', () => {
+  it('shoud call UpdateMarket usecase with correct values', async () => {
     // Arrange
+    const { sut, mockedUpdateMarket } = makeSut();
+    const updateMarketSpy = vi.spyOn(mockedUpdateMarket, 'execute');
+    const { params } = mockedUpdateMarketControllerParams();
+
     // Act
+    await sut.handle(params);
+
     // Assert
+    expect(updateMarketSpy).toHaveBeenCalledWith(params);
   });
 
   it.todo('shoud return 404 - NotFound if UpdateMarket usecase return MarketNotFoundError', () => {
