b2a5794a19e163fd6d9cdbc1214cfbfb6885e241
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-20 00:48:09 -0300

    feat: add UpdateMarketRepository implementation

--- src/infrastructure/prisma/market-repository.ts
+++ src/infrastructure/prisma/market-repository.ts
@@ -6,13 +6,15 @@ import {
   GetMarketByIdRepository,
   GetMarketByIdRepositoryParams,
   NewMarketRepository,
+  UpdateMarketRepository,
 } from '@/application';
 import { Market } from '@/domain';
 import { prisma } from '@/main/prisma/client';
 
 export type MarketRepositories = GetMarketByCodeRepository &
   GetMarketByIdRepository &
-  NewMarketRepository;
+  NewMarketRepository &
+  UpdateMarketRepository;
 
 export class PrismaMarketRepository implements MarketRepositories {
   getById = async ({ id }: GetMarketByIdRepositoryParams): Promise<Market | undefined> => {
@@ -37,6 +39,15 @@ export class PrismaMarketRepository implements MarketRepositories {
 
   new = async (market: Market): Promise<void> => {
     await prisma.market.create({
+      data: MarketMapper.toPersistence(market),
+    });
+  };
+
+  update = async (market: Market): Promise<void> => {
+    await prisma.market.update({
+      where: {
+        id: market.id,
+      },
       data: MarketMapper.toPersistence(market),
     });
   };
