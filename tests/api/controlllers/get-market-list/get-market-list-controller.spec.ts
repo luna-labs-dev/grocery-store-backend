0c7adc9a199a6ab58fa8cb44a004f8dec0bcb538
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-22 20:42:01 -0300

    test: add base test list to getMarketList controller

---
+++ tests/api/controlllers/get-market-list/get-market-list-controller.spec.ts
@@ -0,0 +1,15 @@
+describe('GetMarketListController', () => {
+  it.todo('shoud call GetMarketList usecase with correct values', () => {});
+
+  it.todo(
+    'shoud return 404 - NotFound if GetMarketList usecase return MarketNotFoundError',
+    () => {},
+  );
+
+  it.todo(
+    'shoud return 500 - ServerError if GetMarketList usecase return UnexpectedError',
+    () => {},
+  );
+
+  it.todo('shoud return 200 - Ok on success', () => {});
+});
