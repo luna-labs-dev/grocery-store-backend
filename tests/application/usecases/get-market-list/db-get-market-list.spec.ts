e06d7f1f0ca767fde5fbc82d0c5cd584ad4a3cb2
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-22 01:59:12 -0300

    test: add DbGetMarketList test list (todo)

---
+++ tests/application/usecases/get-market-list/db-get-market-list.spec.ts
@@ -0,0 +1,8 @@
+describe('DbGetMarketList', () => {
+  it.todo('shoud call GetMarketListRepository.count with correct values', () => {});
+  it.todo('shoud return UnexpectedError if GetMarketListRepository.count throws', () => {});
+  it.todo('shoud an empty list if GetMarketListRepository.count returns 0', () => {});
+  it.todo('shoud call GetMarketListRepository.getAll with correct values', () => {});
+  it.todo('shoud return UnexpectedError if GetMarketListRepository.getAll throws', () => {});
+  it.todo('shoud return a market list on success', () => {});
+});
