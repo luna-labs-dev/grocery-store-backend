42839e6bb0a84b37779f7dbd59ff60b6390fb8c1
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-22 02:23:53 -0300

    test: ensure DbGetMarketList returns UnexpectedError if GetMarketListRepository.count throws

--- tests/application/usecases/get-market-list/db-get-market-list.spec.ts
+++ tests/application/usecases/get-market-list/db-get-market-list.spec.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import { MakeSut, mockGetMarketList } from './mockes';
 
+import { left, UnexpectedError } from '@/domain';
+
 describe('DbGetMarketList', () => {
   it('shoud call GetMarketListRepository.count with correct values', async () => {
     // Arrange
@@ -15,9 +17,26 @@ describe('DbGetMarketList', () => {
       search,
     });
   });
-  it.todo('shoud return UnexpectedError if GetMarketListRepository.count throws', () => {});
+
+  it('shoud return UnexpectedError if GetMarketListRepository.count throws', async () => {
+    // Arrange
+    const { sut, mockedMarketRepository } = MakeSut();
+    vi.spyOn(mockedMarketRepository, 'count').mockImplementationOnce(() => {
+      throw new Error('something went wrong with the database');
+    });
+    const { paginatedParams } = mockGetMarketList();
+    // Act
+    const response = await sut.execute(paginatedParams);
+
+    // Assert
+    expect(response).toEqual(left(new UnexpectedError()));
+  });
+
   it.todo('shoud an empty list if GetMarketListRepository.count returns 0', () => {});
+
   it.todo('shoud call GetMarketListRepository.getAll with correct values', () => {});
+
   it.todo('shoud return UnexpectedError if GetMarketListRepository.getAll throws', () => {});
+
   it.todo('shoud return a market list on success', () => {});
 });
