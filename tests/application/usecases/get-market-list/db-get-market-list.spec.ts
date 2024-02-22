1cee2b8e5262437f67483ca42916d445bdd1b8df
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-22 02:13:58 -0300

    test: ensure GetMarketListRepository.count is being called with the right values

--- tests/application/usecases/get-market-list/db-get-market-list.spec.ts
+++ tests/application/usecases/get-market-list/db-get-market-list.spec.ts
@@ -1,5 +1,26 @@
+import 'reflect-metadata';
+import { MakeSut } from './mockes';
+
 describe('DbGetMarketList', () => {
-  it.todo('shoud call GetMarketListRepository.count with correct values', () => {});
+  it('shoud call GetMarketListRepository.count with correct values', async () => {
+    // Arrange
+    const { sut, mockedMarketRepository } = MakeSut();
+    const repositorySpy = vi.spyOn(mockedMarketRepository, 'count');
+
+    // Act
+    await sut.execute({
+      pageIndex: 0,
+      pageSize: 20,
+      orderBy: 'createdAt',
+      orderDirection: 'asc',
+      search: 'Assai',
+    });
+
+    // Assert
+    expect(repositorySpy).toHaveBeenCalledWith({
+      search: 'Assai',
+    });
+  });
   it.todo('shoud return UnexpectedError if GetMarketListRepository.count throws', () => {});
   it.todo('shoud an empty list if GetMarketListRepository.count returns 0', () => {});
   it.todo('shoud call GetMarketListRepository.getAll with correct values', () => {});
