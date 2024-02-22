1d5713e8c0ec8ad46262667dba99822edc4d5cb4
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-22 19:41:17 -0300

    feat: ensure DbGetMarketList calls GetMarketListRepository.getAll with correct values

--- src/application/usecases/db-get-market-list.ts
+++ src/application/usecases/db-get-market-list.ts
@@ -8,7 +8,6 @@ import {
   GetMarketListParams,
   GetMarketListResult,
   left,
-  Market,
   right,
   UnexpectedError,
 } from '@/domain';
@@ -25,31 +24,34 @@ export class DbGetMarketList implements GetMarketList {
     orderDirection,
   }: GetMarketListParams): Promise<Either<UnexpectedError, GetMarketListResult>> => {
     try {
+      // Count the Total
       const marketCount = await this.repositories.count({
         search,
       });
 
+      // Create response object with the count
       const response: GetMarketListResult = {
         total: marketCount,
         markets: [],
       };
 
-      if (!marketCount) {
-        return right(response);
+      if (marketCount > 0) {
+        await this.repositories.getAll({
+          search,
+          pageIndex,
+          pageSize,
+          orderBy,
+          orderDirection,
+        });
       }
 
-      return await Promise.resolve(
-        right({
-          total: 1,
-          markets: [
-            Market.create({
-              name: 'Assai',
-              createdAt: new Date(),
-              createdBy: 'some@email.com',
-            }),
-          ],
-        }),
-      );
+      // Fetch actual Market list only if total is greater than 0
+
+      // -> Set list to the response.markets
+
+      // return the response
+
+      return right(response);
     } catch (error) {
       console.error(error);
 
