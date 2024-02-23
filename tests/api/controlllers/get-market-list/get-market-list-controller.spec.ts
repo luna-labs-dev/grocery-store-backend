cbfeac477554b7c5692b3df33dfc774075bd81f6
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-23 00:26:04 -0300

    test: ensure GetMarketListController returns 404 if GetMarketList returns MarketNotFoundError

--- tests/api/controlllers/get-market-list/get-market-list-controller.spec.ts
+++ tests/api/controlllers/get-market-list/get-market-list-controller.spec.ts
@@ -2,6 +2,9 @@ import 'reflect-metadata';
 
 import { makeSut, mockedGetMarketListControllerObjects } from './mocks';
 
+import { left, MarketNotFoundError } from '@/domain';
+import { notFound } from '@/api';
+
 describe('GetMarketListController', () => {
   it('shoud call GetMarketList usecase with correct values', async () => {
     // Arrange
@@ -16,10 +19,18 @@ describe('GetMarketListController', () => {
     expect(getMarketListSpy).toHaveBeenCalledWith(params);
   });
 
-  it.todo(
-    'shoud return 404 - NotFound if GetMarketList usecase return MarketNotFoundError',
-    () => {},
-  );
+  it('shoud return 404 - NotFound if GetMarketList usecase return MarketNotFoundError', async () => {
+    // Arrange
+    const { sut, mockedGetMarketList } = makeSut();
+    vi.spyOn(mockedGetMarketList, 'execute').mockResolvedValueOnce(left(new MarketNotFoundError()));
+    const { params } = mockedGetMarketListControllerObjects();
+
+    // Act
+    const response = await sut.handle(params);
+
+    // Assert
+    expect(response).toEqual(notFound(new MarketNotFoundError()));
+  });
 
   it.todo(
     'shoud return 500 - ServerError if GetMarketList usecase return UnexpectedError',
