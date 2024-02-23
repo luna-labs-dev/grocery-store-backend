d5ce07502ecf3892053c7f659920f24b6e67c600
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-23 01:01:48 -0300

    feat: add getMarketListController route

--- src/main/routes/market-routes.ts
+++ src/main/routes/market-routes.ts
@@ -6,5 +6,6 @@ import { injection } from '../di';
 
 export const marketRouter = Router();
 
+marketRouter.get('/', adaptRoute(container.resolve(injection.controllers.getMarketList)));
 marketRouter.post('/', adaptRoute(container.resolve(injection.controllers.newMarket)));
 marketRouter.put('/:marketId', adaptRoute(container.resolve(injection.controllers.updateMarket)));
