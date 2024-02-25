4d0fcbafb8bec16fcb83719f48fe880589752918
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-25 03:30:53 -0300

    test: add StartShoppingEvent base test file (filled only with the todos

---
+++ tests/application/usecases/start-shopping-event/db-start-shopping-event.spec.ts
@@ -0,0 +1,13 @@
+describe('DbStartShoppingEvent', () => {
+  it.todo('shoud call GetMarketById with correct id', () => {});
+
+  it.todo('shoud return UnexpectedError if GetMarketById throws', () => {});
+
+  it.todo('shoud return MarketNotFoundError if GetMarketById returns undefined', () => {});
+
+  it.todo('shoud call AddShoppingEvent with correct values', () => {});
+
+  it.todo('shoud return UnexpectedError if AddShoppingEvent throws', () => {});
+
+  it.todo('shoud return the created shoppingEvent with the correct values', () => {});
+});
