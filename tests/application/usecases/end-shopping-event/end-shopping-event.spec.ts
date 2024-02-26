285e98e503ebf70d02b3a05a03eea23bed5e026d
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-26 20:30:55 -0300

    test: add base EndShoppingEvent usecase test file

---
+++ tests/application/usecases/end-shopping-event/end-shopping-event.spec.ts
@@ -0,0 +1,16 @@
+describe('DbEndShoppingEvent', () => {
+  it.todo('shoud call GetShoppingEventByIdRepository with correct values', () => {});
+
+  it.todo('shoud return UnexpectedError if GetShoppingEventByIdRepository throws', () => {});
+
+  it.todo(
+    'shoud return ShoppingEventNotFoundError if GetShoppingEventByIdRepository returns undefined',
+    () => {},
+  );
+
+  it.todo('should call UpdateShoppingEventRepository with correct values', () => {});
+
+  it.todo('shoud return UnexpectedError if UpdateShoppingEventRepository throws', () => {});
+
+  it.todo('shoud return the ended ShoppingEvent on success', () => {});
+});
