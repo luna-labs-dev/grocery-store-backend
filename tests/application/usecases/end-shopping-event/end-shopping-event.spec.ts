cf101fe896d2bea8457245be2638e376cd3c8ad6
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-02-27 00:03:32 -0300

    test: ensure DbEndShoppingEvent calls GetShoppingEventByIdRepository with correct values

--- tests/application/usecases/end-shopping-event/end-shopping-event.spec.ts
+++ tests/application/usecases/end-shopping-event/end-shopping-event.spec.ts
@@ -1,5 +1,24 @@
+import 'reflect-metadata';
+
+import { makeSut } from './mocks';
+
 describe('DbEndShoppingEvent', () => {
-  it.todo('shoud call GetShoppingEventByIdRepository with correct values', () => {});
+  it('shoud call GetShoppingEventByIdRepository with correct values', async () => {
+    // Arrange
+    const { sut, mockedShoppingEventRepository } = makeSut();
+
+    const shoppingEventRepositorySpy = vi.spyOn(mockedShoppingEventRepository, 'getById');
+
+    // Act
+    await sut.execute({
+      shoppingEventId: '09cde9b9-b54e-4c8a-b87b-f9efec57c893',
+    });
+
+    // Assert
+    expect(shoppingEventRepositorySpy).toHaveBeenCalledWith({
+      shoppingEventId: '09cde9b9-b54e-4c8a-b87b-f9efec57c893',
+    });
+  });
 
   it.todo('shoud return UnexpectedError if GetShoppingEventByIdRepository throws', () => {});
 
--- tests/application/usecases/end-shopping-event/mocks.ts
+++ tests/application/usecases/end-shopping-event/mocks.ts
@@ -1,17 +1,21 @@
 import { mockRepositories } from 'tests/mocks/repositories';
 
-import { AddShoppingEventRepository, DbEndShoppingEvent } from '@/application';
+import {
+  DbEndShoppingEvent,
+  GetShoppingEventByIdRepository,
+  UpdateShoppingEventRepository,
+} from '@/application';
 import { EndShoppingEvent } from '@/domain';
 
 interface SutResult {
   sut: EndShoppingEvent;
-  mockedShoppingEventRepository: AddShoppingEventRepository;
+  mockedShoppingEventRepository: GetShoppingEventByIdRepository & UpdateShoppingEventRepository;
 }
 
 export const makeSut = (): SutResult => {
   const { mockedShoppingEventRepository } = mockRepositories();
 
-  const sut = new DbEndShoppingEvent();
+  const sut = new DbEndShoppingEvent(mockedShoppingEventRepository);
 
   return {
     sut,
