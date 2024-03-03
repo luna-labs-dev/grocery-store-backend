b46028f3282ac7992b57ff0b0ebd19f003974c7a
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-03-03 03:22:09 -0300

    feat: add GetShoppingEventList usecase full implementation

--- src/application/usecases/db-get-shopping-event-list.ts
+++ src/application/usecases/db-get-shopping-event-list.ts
@@ -7,6 +7,7 @@ import {
   GetShoppingEventListPossibleErrors,
   GetShoppingEventListResult,
   left,
+  right,
   UnexpectedError,
 } from '@/domain';
 
@@ -15,21 +16,40 @@ export class DbGetShoppingEventList implements GetShoppingEventList {
   execute = async ({
     status,
     period,
+    pageIndex,
+    pageSize,
+    orderBy,
+    orderDirection,
   }: GetShoppingEventListParams): Promise<
     Either<GetShoppingEventListPossibleErrors, GetShoppingEventListResult>
   > => {
     try {
       // Get the ShoppingEvent List count based on the filters
-      await this.repository.count({ status, period });
+      const shoppingEventListCount = await this.repository.count({ status, period });
 
       // Create the return object with the count and an empty array
+      const response: GetShoppingEventListResult = {
+        total: shoppingEventListCount,
+        shoppingEvents: [],
+      };
 
       // IF count is greater than 0 fetch the actual list based on the same filter
+      if (shoppingEventListCount > 0) {
+        const shoppingEventList = await this.repository.getAll({
+          status,
+          period,
+          pageIndex,
+          pageSize,
+          orderBy,
+          orderDirection,
+        });
 
-      // Assign the returned list in the shoppingList array inside the return object
+        // Assign the returned list in the shoppingList array inside the return object
+        response.shoppingEvents = shoppingEventList;
+      }
 
       // Return the object
-      return left(new UnexpectedError());
+      return right(response);
     } catch (error) {
       console.error(error);
 
