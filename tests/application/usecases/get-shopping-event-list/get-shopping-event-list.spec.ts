92b4efa73395023f7a0ff2f4f47ecdaec4778590
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-03-03 03:15:25 -0300

    test: ensure GetShoppingEventList returns UnexpectedError if GetShoppingEventListRepo.count throws

--- tests/application/usecases/get-shopping-event-list/get-shopping-event-list.spec.ts
+++ tests/application/usecases/get-shopping-event-list/get-shopping-event-list.spec.ts
@@ -1,6 +1,10 @@
 import 'reflect-metadata';
 
+import { databaseError } from 'tests/mocks/errors';
+
 import { makeSut, mockEndShoppingEventData } from './mocks';
+
+import { left, UnexpectedError } from '@/domain';
 
 describe('GetShoppingEventList', () => {
   it('shoud call GetShoppingEventListRepository.count with the correct values', async () => {
@@ -18,7 +22,20 @@ describe('GetShoppingEventList', () => {
     expect(shoppingEventRepositorySpy).toHaveBeenCalledWith(repositoryParams);
   });
 
-  it.todo('shoud return UnexpectedError if GetShoppingEventListRepository.count throws', () => {});
+  it('shoud return UnexpectedError if GetShoppingEventListRepository.count throws', async () => {
+    // Arrange
+    const { sut, mockedShoppingEventRepository } = makeSut();
+
+    vi.spyOn(mockedShoppingEventRepository, 'count').mockImplementationOnce(databaseError);
+
+    const { params } = mockEndShoppingEventData();
+
+    // Act
+    const response = await sut.execute(params);
+
+    // Assert
+    expect(response).toEqual(left(new UnexpectedError()));
+  });
 
   it.todo(
     'shoud return the list with an empty array if GetShoppingEventListRepository.count returns 0',
