42fabf7ffdfee245886b4330f0de2381caed479d
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-03-03 03:15:15 -0300

    feat: ensure GetShoppingEventList returns UnexpectedError if GetShoppingEventListRepo.count throws

--- src/application/usecases/db-get-shopping-event-list.ts
+++ src/application/usecases/db-get-shopping-event-list.ts
@@ -18,16 +18,22 @@ export class DbGetShoppingEventList implements GetShoppingEventList {
   }: GetShoppingEventListParams): Promise<
     Either<GetShoppingEventListPossibleErrors, GetShoppingEventListResult>
   > => {
-    // Get the ShoppingEvent List count based on the filters
-    await this.repository.count({ status, period });
+    try {
+      // Get the ShoppingEvent List count based on the filters
+      await this.repository.count({ status, period });
 
-    // Create the return object with the count and an empty array
+      // Create the return object with the count and an empty array
 
-    // IF count is greater than 0 fetch the actual list based on the same filter
+      // IF count is greater than 0 fetch the actual list based on the same filter
 
-    // Assign the returned list in the shoppingList array inside the return object
+      // Assign the returned list in the shoppingList array inside the return object
 
-    // Return the object
-    return left(new UnexpectedError());
+      // Return the object
+      return left(new UnexpectedError());
+    } catch (error) {
+      console.error(error);
+
+      return left(new UnexpectedError());
+    }
   };
 }
