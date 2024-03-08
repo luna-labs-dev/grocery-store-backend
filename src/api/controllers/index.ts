3c7996726b1f16ea1d4f8e480d0feed122b73de4
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-03-08 19:10:30 -0300

    feat: add AddProductToCartController

--- src/api/controllers/index.ts
+++ src/api/controllers/index.ts
@@ -5,3 +5,4 @@ export * from './start-shopping-event';
 export * from './end-shopping-event';
 export * from './get-shopping-event-list';
 export * from './get-shopping-event-by-id';
+export * from './add-product-to-cart';
