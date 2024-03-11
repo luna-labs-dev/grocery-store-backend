628c01c9b1a4c12cab5e9418324046d1311da8b8
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-03-11 19:31:53 -0300

    feat: add UpdateProductInCart usecase interface

--- src/domain/usecases/errors/index.ts
+++ src/domain/usecases/errors/index.ts
@@ -2,3 +2,4 @@ export * from './unexpected-error';
 export * from './market-already-exists-error';
 export * from './market-not-found-error';
 export * from './shopping-event-not-found-error';
+export * from './product-not-found-error';
---
+++ src/domain/usecases/update-product-in-cart.ts
@@ -0,0 +1,21 @@
+import { Either } from '../core';
+import { Product } from '../entities';
+
+import { ProductNotFoundError, UnexpectedError } from './errors';
+
+export type UpdateProductInCartErrors = UnexpectedError & ProductNotFoundError;
+
+export interface UpdateProductInCartParams {
+  shoppingEventId: string;
+  name: string;
+  amount: number;
+  wholesaleMinAmount?: number;
+  price: number;
+  wholesalePrice?: number;
+}
+
+export interface UpdateProductInCart {
+  execute: (
+    params: UpdateProductInCartParams,
+  ) => Promise<Either<UpdateProductInCartErrors, Product>>;
+}
