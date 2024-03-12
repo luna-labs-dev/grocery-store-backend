7d929e9eee78c58dfc4656c1e379711876f4147c
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-03-11 21:23:08 -0300

    feat: add UpdateProductInCart usecase base implementation

--- src/application/usecases/db-update-product-in-cart.ts
+++ src/application/usecases/db-update-product-in-cart.ts
@@ -1,33 +1,71 @@
+import { GetShoppingEventByIdRepository, UpdateShoppingEventRepository } from '../contracts';
+
 import {
   Either,
   left,
   Product,
+  ProductNotFoundError,
+  right,
+  ShoppingEventNotFoundError,
   UnexpectedError,
   UpdateProductInCart,
   UpdateProductInCartErrors,
   UpdateProductInCartParams,
 } from '@/domain';
 
+type UpdateProductInCartRepositories = GetShoppingEventByIdRepository &
+  UpdateShoppingEventRepository;
+
 export class DbUpdateProductInCart implements UpdateProductInCart {
+  constructor(private readonly repository: UpdateProductInCartRepositories) {}
+
   execute = async ({
     shoppingEventId,
+    productId,
     name,
     amount,
     price,
     wholesaleMinAmount,
     wholesalePrice,
   }: UpdateProductInCartParams): Promise<Either<UpdateProductInCartErrors, Product>> => {
-    // Fetch shoppingEvent
+    try {
+      // Fetch shoppingEvent
+      const shoppingEvent = await this.repository.getById({
+        shoppingEventId,
+      });
 
-    // Return shoppingEventNotFoundError if shoppingEvent is undefined
+      // Return shoppingEventNotFoundError if shoppingEvent is undefined
+      if (!shoppingEvent) {
+        return left(new ShoppingEventNotFoundError());
+      }
 
-    // Return productNotFoundError if product not in list
+      // Return productNotFoundError if product not in list
+      const product = shoppingEvent.products.getItemById(productId);
 
-    // Update product with new values
+      if (!product) {
+        return left(new ProductNotFoundError());
+      }
 
-    // Update shoppingEvent in database
+      // Update product with new values
+      product.update({
+        name,
+        amount,
+        price,
+        wholesaleMinAmount,
+        wholesalePrice,
+      });
 
-    // return product object
-    return await Promise.resolve(left(new UnexpectedError()));
+      shoppingEvent.products.add(product);
+
+      // Update shoppingEvent in database
+      await this.repository.update(shoppingEvent);
+
+      // return product object
+      return right(product);
+    } catch (error) {
+      console.error(error);
+
+      return left(new UnexpectedError());
+    }
   };
 }
--- src/domain/usecases/update-product-in-cart.ts
+++ src/domain/usecases/update-product-in-cart.ts
@@ -7,6 +7,7 @@ export type UpdateProductInCartErrors = UnexpectedError & ProductNotFoundError;
 
 export interface UpdateProductInCartParams {
   shoppingEventId: string;
+  productId: string;
   name: string;
   amount: number;
   wholesaleMinAmount?: number;
