ae53120931c5558462600ad1791a0b8039c77a93
Tiago Luiz Seuaciuc de Poli <tiago.poli@stoneage.com.br>
2024-03-11 22:02:34 -0300

    feat: add UpdateProductInCartController

---
+++ src/api/controllers/update-product-in-cart/update-product-in-cart-controller-validation-schema.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const UpdateProductInCartRequestSchema = z.object({
+  shoppingEventId: z.string().uuid(),
+  productId: z.string().uuid(),
+  name: z.string().min(1),
+  amount: z.number().int().gt(0),
+  price: z.number().gt(0),
+  wholesaleMinAmount: z.number().int().gt(0).optional(),
+  wholesalePrice: z.number().gt(0).optional(),
+});
