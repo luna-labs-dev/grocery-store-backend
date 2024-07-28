2103e280474e5e76941c557d553d5188f7d572a9
Tiago Poli <tiagoluizpoli@gmail.com>
2024-07-28 20:57:42 -0300

    refactor: run migration

---
+++ prisma/migrations/20240728235716_remove_displayname_from_user_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `displayName` on the `user` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "user" DROP COLUMN "displayName";
