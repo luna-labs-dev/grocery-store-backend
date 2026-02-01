4e42ec6e0f388c4ea06c7c61a32258232bf1309f
Tiago Poli <tiagoluizpoli@gmail.com>
2026-02-01 02:05:33 -0300

    chore: small setup changes

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
-import tsconfigPaths from 'vite-tsconfig-paths'
-import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
-    testTimeout: 60 * 1000
+    testTimeout: 60 * 1000,
   },
-  plugins: [tsconfigPaths()]
+  plugins: [tsconfigPaths()],
 });
